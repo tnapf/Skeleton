1e5deafce9f3bb9d877c8fd1084cba12872b7767
Robert Snedeker <44886996+CommandString@users.noreply.github.com>
2023-02-13 08:40:58 -0500

    Added getMimeFromExtension

--- common/Helpers.php
+++ common/Helpers.php
@@ -7,4 +7,19 @@ class Helpers {
     {
         return file_get_contents(__DIR__."/../views/{$view}");
     }
+
+    public static function getMimeFromExtension(string $extensionToFindMimeFor): ?string
+    {
+        $mimes = json_decode(file_get_contents(__DIR__ . "/Mimes.json"));
+
+        foreach ($mimes as $mime => $extensions) {
+            foreach ($extensions as $extension) {
+                if ($extension == $extensionToFindMimeFor) {
+                    return $mime;
+                }
+            }
+        }
+
+        return null;
+    }
 }
