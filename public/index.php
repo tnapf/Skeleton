8a4df7cd258893ecfc1810a5561729699e9e05ee
Robert Snedeker <44886996+CommandString@users.noreply.github.com>
2023-02-13 08:41:41 -0500

    Fixed CSS returning as text/plain

--- public/index.php
+++ public/index.php
@@ -1,23 +1,24 @@
 <?php
 
+use Common\Helpers;
 use Controllers\ErrorHandlers;
 use Controllers\Home;
 use Tnapf\Router\Exceptions\HttpInternalServerError;
 use Tnapf\Router\Exceptions\HttpNotFound;
 use Tnapf\Router\Router;
 
+require_once __DIR__."/../vendor/autoload.php";
+
 if (PHP_SAPI === "cli-server") {
     $url = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
     $path = pathinfo($url);
     if (!empty($path["extension"])) {
         $file_path = __DIR__."{$path['dirname']}/{$path["basename"]}";
-        header("content-type: ".mime_content_type($file_path));
+        header("content-type: ".Helpers::getMimeFromExtension($path["extension"]));
         readfile($file_path);
         return;
     }
 }
-
-require_once __DIR__."/../vendor/autoload.php";
 
 Router::get("/", [Home::class, "handle"]);
 
